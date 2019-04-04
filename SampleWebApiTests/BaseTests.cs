097c6b9676744a7dfb44309dbcab86ce57ab779a
Matt Ruma <mattruma@gmail.com>
2019-04-04 15:12:56 -0400

    add other test projects

---
+++ SampleWebApiTests/BaseTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleWebApiTests
+{
+    public class BaseTests
+    {
+        protected IConfiguration _configuration;
+
+        [SetUp]
+        public void Setup()
+        {
+            _configuration = new ConfigurationBuilder()
+                  .AddJsonFile("appsettings.json")
+                  .AddJsonFile("appsettings.Development.json")
+                  .Build();
+        }
+    }
+}
