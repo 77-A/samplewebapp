ebbdbb02a8905446924b0f3d5667c659dc5e4458
Matt Ruma <mattruma@gmail.com>
2019-04-04 13:12:42 -0400

    initial commit

---
+++ SampleWebApi/Controllers/SampleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/SampleController")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SampleController(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet()]
+        public ActionResult<string> Get(string name)
+        {
+            return new OkObjectResult(_configuration[name]);
+        }
+    }
+}
---
+++ SampleWebApp/Pages/Sample.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleWebApp.Pages
+{
+    public class SampleModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public string Value { get; set; }
+
+        public SampleModel(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet(string name)
+        {
+            this.Value = _configuration[name];
+        }
+    }
+}
